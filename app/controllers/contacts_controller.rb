5742efc5bd00a1a53d7891eda7fef0c4d5e483bd
Arkadiusz Karpinski <arekkarpinski2@gmail.com>
2021-03-10 16:25:25 +0100

    getting contacts from api connector

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,10 @@
 class ContactsController < ApplicationController
   def index
-    @contacts
+    page_token = 2
+    result = HTTP.get("http://localhost:3000/contacts", json: { page_token: page_token })
+
+    result_json = MultiJson.load(result.body.to_s)
+    @contacts = result_json["data"]
+    
   end
 end
