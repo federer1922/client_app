e723b7f448568849215577bcd83d31eee27f0597
Arkadiusz Karpinski <arekkarpinski2@gmail.com>
2021-03-11 14:15:05 +0100

    displaying next pages

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,41 @@
 class ContactsController < ApplicationController
   def index
-    page_token = 2
+    if params["page_token"]
+      page_token = params["page_token"]
+    else
+      page_token = nil
+    end
+
     result = HTTP.get("http://localhost:3000/contacts", json: { page_token: page_token })
 
     result_json = MultiJson.load(result.body.to_s)
-    @contacts = result_json["data"]
-    
+    @next_page_token = result_json["next_page_token"]
+    @contacts = result_json["data"]   
+  end
+
+  def create
+
+  https://#companyname#.scoro.com/api/contacts/modify 
+{
+    "apiKey": "API_hash",
+    "lang": "eng",
+    "company_account_id": "tutorial",
+    "request": {
+        "name": "Testing 123",
+        "lastname": "",
+        "bankaccount": null,
+        "birthday": "0000-00-00",
+        "position": "",
+        "comments": "",
+        "sex": null,
+        "id_code": null,
+        "is_client": null,
+        "is_supplier": null,
+        "vatno": null
+    }
+}
+
+
+    redirect_to action: "index"
   end
 end
---
+++ spec/controllers/contacts_controller_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+describe ContactsController, type: :controller do
+
+  it "" do
+  
+
+  end
+end
